70d54d0ee88691d8686e1eae3125d985f96b8f1c
Peter Upfold <strategyoracle+github@gmail.com>
2015-08-24 18:30:37 +0100

    Added whitelist reset script

---
+++ zzzzzz-cuu-whitelistreset.cron.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Cautious Unattended Upgrades -- Client Whitelist Rest
+#
+#
+# This must be run by cron.daily as the **LAST** script it executes -- so perhaps
+# called /etc/cron.daily/zzzzzz-cuu-whitelistreset
+#
+#
+
+sed -i -e 's/Package-Whitelist {\([^}]\+\)}/Package-Whitelist { }/g' /etc/apt/apt.conf.d/50unattended-upgrades
